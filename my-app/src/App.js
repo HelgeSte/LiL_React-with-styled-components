a6565f151ada3dd860bbfb3a4e079ffab650e837
Helge Stegemoen <helge@stegemoen.com>
2025-08-17 12:57:30 +0200

    Using export and import for button, footer and header. Install and import axios package. useState and useEffects. Simple prop usage.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Button from "./components/examples/Button";
+import Header from "./components/examples/Header";
+import Footer from "./components/examples/Footer";
+import axios from "axios";
+import {useState, useEffect} from "react";
 
-function App() {
+const App = () => {
+  const [data, setData]= useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response =>
+      setData(response.data))
+      .catch(error =>
+        console.error('Error fetching data', error));
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div>
+        <Header />
+        <h1>My React App</h1>
+        <ul>
+          {data.map (post => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+        <Button label="Click me" />
+        <Footer />
+      </div>
   );
-}
+};
 
 export default App;
