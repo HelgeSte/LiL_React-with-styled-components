818f2125f4f9b743f1eec45d4d6517068c4babad
Helge Stegemoen <helge@stegemoen.com>
2025-08-27 08:58:44 +0200

    Used spread for props

--- my-app/src/examples/Parent.js
+++ my-app/src/examples/Parent.js
@@ -6,10 +6,17 @@ const Parent = () => {
     const propsObject = {
         heading: 'Welcome to React Spreading',
         description: 'Learn about prop spreading in react',
+        /* After preading, these properties becomes props.heading and props.description in
+           SpreadPropsComponent.js, and saves us from typing it like this:
+                <SpreadPropsComponent heading={propsObject.heading} 
+                    description={propsObject.description} />
+            This is specially useful when you have a large number of props or
+            when the props are dynamic.
+        */
     }
     return (
         <>
-            <SpreadPropsComponent {...propsObject} />
+            <SpreadPropsComponent {...propsObject} /> 
         </>
     );
 };
