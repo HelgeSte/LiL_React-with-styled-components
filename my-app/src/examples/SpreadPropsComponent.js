adae74000a380a14c6955cd2abc9ecb96d41e896
Helge Stegemoen <helge@stegemoen.com>
2025-08-27 13:21:03 +0200

    Replaced properties with a api call

--- my-app/src/examples/SpreadPropsComponent.js
+++ my-app/src/examples/SpreadPropsComponent.js
@@ -12,6 +12,13 @@ const SpreadPropsComponent = (props) => {
     );
 };
 
+// propTypes is a static property on a React component that defines the //
+// type and sometimes the required nature of each prop it expects to receive. 
+// When you use it, React will check the props passed to your component against 
+// the definitions you've provided. If the types don't match, it will issue a 
+// warning in the development console. This is incredibly helpful for catching 
+// errors early, especially in large applications where multiple developers are 
+// working on the same codebase.
 SpreadPropsComponent.propTypes = {
     heading: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
